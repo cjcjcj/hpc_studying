4b99cc7165fe91586cd5ae208685d2359c06222b
ailin <alexander.ilin.92@gmail.com>
2017-06-13 16:11:20 +0300

    back to approaches, namespaces, works on std::thread

--- pp/nbody/main.cc
+++ pp/nbody/main.cc
@@ -1,6 +1,6 @@
 #include "float3.h"
 #include "body.h"
-#include "sequential.h"
+#include "approaches.h"
 #include "general.h"
 
 #include <cmath>
@@ -13,11 +13,14 @@ int main()
         simulation_steps = 5,
         repeats = 5;
 
+    simulation::stdthread::simulate(10000, 2);
+    return 1;
+
     for (int repeat = 0; repeat < repeats; repeat++)
     {
         std::cout << "cycle #" << repeat << std::endl;
         for(int pow = 1; pow <= max_pow; pow++)
-            sequential(std::pow(10, pow), simulation_steps);
+            simulation::stdthread::simulate(std::pow(10, pow), simulation_steps);
         std::cout << std::endl;
     }
 }
